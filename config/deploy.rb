314a6f1425701a7b95f9d616efad050b0ff6b206
Jason L Perry <jasper@ambethia.com>
2008-10-18 06:21:09 -0400

    Deployed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,34 @@
-default_run_options[:pty] = true
 set :application, "code_tennis"
+
+set :deploy_to, "/var/apps/#{application}"
+
+set :scm, :git
 set :repository,  "git@github.com:railsrumble/tampa-rb.git"
-set :scm, "git"
-#set :scm_passphrase
-set :user, "deploy"
-ssh_options[:forward_agent] = true
-set :branch, "master"
-
-set :deploy_to, "/var/www/vhosts/#{application}"
-
+set :git_enable_submodules, true
+set :deploy_via, :remote_cache
 
 role :app, "66.246.75.37"
 role :web, "66.246.75.37"
 role :db,  "66.246.75.37", :primary => true
+
+ssh_options[:username] = "deploy"
+set :run_method, :run
+
+namespace :deploy do
+  task :custom_symlinks do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  
+  desc "Restart Passenger"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with Passenger"
+    task t, :roles => :app do ; end
+  end
+end
+
+after "deploy:symlink", "deploy:custom_symlinks"
+after "deploy", "deploy:cleanup"
