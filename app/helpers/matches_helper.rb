c484c0ad7cad1f6f52ca146224b32f519c03da3e
Matt Cowley <madcowley@gmail.com>
2008-10-19 09:28:52 -0400

    timeline

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,10 @@
 module MatchesHelper
+  
+  def commit_messages(volley)
+    html = ''
+    volley.commits.each do |commit|
+      html << commit.message << "  --  " << commit.created_at.strftime("%b %d %H:%M") <<  "<br />"
+    end
+    html
+  end
 end
