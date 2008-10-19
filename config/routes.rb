5138ee9b599b46dee71494e159333f3acf9a1a74
Matt Cowley <madcowley@gmail.com>
2008-10-19 00:08:55 -0400

    timeline

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :matches, :member => { "push" => :post, "complete_volley" => :get, "complete_match" => :get}
+  map.resources :matches, :member => { "push" => :post, "complete_volley" => :get, "complete_match" => :get, "volleys" => :get}
 
   map.resource :session
   map.logout   "/logout", :controller => "sessions", :action => "destroy"
